f8aaca1ecbcde3cabe5067706b948dfe8a521370
nmcuong2502 <nmcuong2502@clc.fitus.edu.vn>
2026-08-11 04:56:00 +0700

    feat(rdt): complete stop-and-wait UDP engine and unit tests

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Admin/.vscode/PJ-SOCKET
+# Build directory: C:/Users/Admin/.vscode/PJ-SOCKET/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test("packet_wire" "C:/Users/Admin/.vscode/PJ-SOCKET/build/packet_tests.exe")
+set_tests_properties("packet_wire" PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Admin/.vscode/PJ-SOCKET/CMakeLists.txt;35;add_test;C:/Users/Admin/.vscode/PJ-SOCKET/CMakeLists.txt;0;")
+add_test("rdt_loopback" "C:/Users/Admin/.vscode/PJ-SOCKET/build/rdt_tests.exe")
+set_tests_properties("rdt_loopback" PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Admin/.vscode/PJ-SOCKET/CMakeLists.txt;43;add_test;C:/Users/Admin/.vscode/PJ-SOCKET/CMakeLists.txt;0;")
